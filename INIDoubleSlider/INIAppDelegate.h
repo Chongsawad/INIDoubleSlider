9200854061c15d46b97fd98bdb8ff39293f504f8
Chongsawad Saiaram <chongsawad252@gmail.com>
2013-12-21 13:47:59 +0700

    Initial Commit

---
+++ INIDoubleSlider/INIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  INIAppDelegate.h
+//  INIDoubleSlider
+//
+//  Created by InICe on 12/21/13.
+//  Copyright (c) 2013 Chongsawad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface INIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ INIDoubleSlider/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  INIDoubleSlider
+//
+//  Created by InICe on 12/21/13.
+//  Copyright (c) 2013 Chongsawad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "INIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([INIAppDelegate class]));
+	}
+}
