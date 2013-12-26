7627f58ce46d0ba143154f21dbc3068293f3dcb9
Chongsawad Saiaram <chongsawad252@gmail.com>
2013-12-26 22:08:53 +0700

    Fix one line text display on range view

--- INIDoubleSlider/Class/INIRangeView.m
+++ INIDoubleSlider/Class/INIRangeView.m
@@ -15,16 +15,15 @@
 {
 	if (self = [super init]) {
 		_range = range;
-
 		UILabel *titleLabel = [[UILabel alloc] init];
-		titleLabel.size = self.size;
-		titleLabel.textAlignment = NSTextAlignmentCenter;
-		titleLabel.numberOfLines = 0;
-		titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
-		titleLabel.minimumScaleFactor = 0;
-		titleLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+		titleLabel.numberOfLines = 1;
 		titleLabel.text = _range.title;
+		titleLabel.textAlignment = NSTextAlignmentCenter;
+		titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+		titleLabel.minimumScaleFactor = 0.5f;
+		titleLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;
 		titleLabel.highlightedTextColor = [UIColor whiteColor];
+		titleLabel.adjustsFontSizeToFitWidth = YES;
 		_titleLabel = titleLabel;
 		[self addSubview:titleLabel];
 	}
@@ -35,6 +34,12 @@
 {
 	_isHighlighted = isHighlighted;
 	[_titleLabel setHighlighted:isHighlighted];
+}
+
+- (void)layoutSubviews
+{
+	[super layoutSubviews];
+	_titleLabel.frame = CGRectInset(self.bounds, 3, 2);
 }
 
 @end
